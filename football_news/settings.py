d3aabfd1ce58decbe1891260f40e6152c2b4f72e
ghiyasFazle <ghiyasfazle30@gmail.com>
2025-09-17 10:39:56 +0700

    tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,8 +30,11 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","ghiyas-fazle-footballnews.pbp.cs.ui.ac.id"]
 
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","ghiyas-fazle-footballnews.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://ghiyas-fazle-footballnews.pbp.cs.ui.ac.id/"
+]
 
 # Application definition
 
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
